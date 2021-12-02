6cc88d5667d586be0dbd4520da33250e30ae01e8
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-12-02 11:47:55 +0000

    feat(day 2): Add day 2 part 2 attempt

--- day2/main.go
+++ day2/main.go
@@ -13,6 +13,8 @@ var input string
 
 func main() {
 	fmt.Printf("The final depth for part one is: %v\n", part1())
+
+	fmt.Printf("The final depth for part two is: %v\n", part2())
 }
 
 func part1() int {
@@ -39,4 +41,33 @@ func part1() int {
 	}
 
 	return horizontal * depth
+}
+
+func part2() int {
+	horizontal := 0
+	depth := 0
+	aim := 0
+
+	moves := strings.Split(input, "\n")
+
+	for _, move := range moves {
+		sections := strings.Split(move, " ")
+		value, err := strconv.Atoi(sections[1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		switch sections[0] {
+		case "up":
+			aim -= value
+		case "down":
+			aim += value
+		case "forward":
+			horizontal += value
+			depth += value * aim
+		}
+	}
+
+	return horizontal * depth
+
 }
