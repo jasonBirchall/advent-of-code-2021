4c9f118b9ec02af4b5eb450a52a9a3d838ad05f5
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-12-06 20:10:07 +0000

    feat(day6): Add day6 part b hashmap

--- day6/main.go
+++ day6/main.go
@@ -13,8 +13,9 @@ var input string
 
 func main() {
 	fmt.Printf("Part A: %d\n", partA(80))
-
+	fmt.Printf("Part B: %d\n", partB(256))
 }
+
 func partA(iterations int) int {
 	s := strings.Split(input, ",")
 	for i := 0; i < iterations; i++ {
@@ -34,4 +35,51 @@ func partA(iterations int) int {
 		}
 	}
 	return len(s)
+}
+
+func partB(iterations int) int {
+	s := strings.Split(input, ",")
+
+	res := make(map[int]int)
+	for _, v := range s {
+		i, err := strconv.Atoi(v)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		res[i] += 1
+	}
+
+	for i := 0; i < iterations; i++ {
+		created := make(map[int]int)
+		for i := 0; i <= 8; i++ {
+
+			k := i
+			v, ok := res[k]
+			if !ok {
+				continue
+			}
+
+			if k == 0 {
+				delete(res, k)
+				created[6] += v
+				created[8] += v
+				continue
+			}
+			res[k] -= v
+			res[k-1] += v
+		}
+
+		for k, v := range created {
+			res[k] += v
+		}
+	}
+
+	var sum int
+
+	for _, v := range res {
+		sum += v
+	}
+
+	return sum
 }
